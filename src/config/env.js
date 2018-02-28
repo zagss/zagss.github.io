b4e2e4bf255a9db1f407b33e9b68c776135c7475
liukai <18211938327@163.com>
2018-02-28 12:08:37 +0800

    up token

--- src/config/env.js
+++ src/config/env.js
@@ -3,7 +3,7 @@ import { convertBlogArticle } from '@/common/js/util'
 const BASEURL = 'https://api.github.com'
 const NAME = 'zagss.github.io'
 const OWNER = 'zagss'
-const ACCESS_TOKEN = 'f7c677e7b3f42aafd6b9c844d41f8b6aa2c44328'
+const ACCESS_TOKEN = '6932b35db6c22c4aca126cdf95225c2d154633e8'
 
 export const GitConfig = {
   BASEURL,
