df7747b84bc0c5e0b6335e49690f11108952cf5f
liukai <18211938327@163.com>
2018-02-28 14:04:18 +0800

    解决token问题

--- static/js/2.2d85395775a23493931b.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2],{nOTH:function(l,e,n){"use strict";Object.defineProperty(e,"__esModule",{value:!0});var u=n("Z0/y")(null,null,!1,null,null,null);e.default=u.exports}});
-//# sourceMappingURL=2.2d85395775a23493931b.js.map
